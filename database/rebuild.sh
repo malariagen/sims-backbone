e349cafb074f61a45ecffadb967d67463fbf2728
Ian Wright <ian.wright@well.ox.ac.uk>
2017-10-12 10:53:45 +0100

    Script to destroy and rebuild the database

--- database/rebuild.sh
+++ database/rebuild.sh
@@ -1,1 +1,10 @@
-sudo -u postgres psql backbone_service < backbone_service.psql
+sudo -u postgres psql << +++EOF
+DROP database backbone_service;
+CREATE DATABASE backbone_service;
+Alter DATABASE backbone_service OWNER TO iwright;
+ALTER DATABASE backbone_service SET search_path=public, backbone_service, contrib;
+\connect backbone_service;
+\i backbone_service.psql;
+\connect backbone_service;
+SELECT postgis_full_version();
++++EOF
