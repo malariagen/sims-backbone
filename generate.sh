6625a861411e238a9385b3c495087ca4066fd245
Ian Wright <ian.wright@well.ox.ac.uk>
2017-10-23 11:35:51 +0100

    Hack no longer required

--- generate.sh
+++ generate.sh
@@ -6,7 +6,6 @@ rm -rf server/bb-server
 cp -pr python-flask-server server/bb-server
 rm -rf typescript-angular2-client
 java -jar swagger-codegen-cli.jar generate -i swagger.yaml -l typescript-angular2 -o typescript-angular2-client -c client.config.json
-sed -i -e 's/models\.Array/Array/' typescript-angular2-client/model/Entities.ts typescript-angular2-client/model/Summary.ts
 rm -rf python_client
 java -jar swagger-codegen-cli.jar generate -i swagger.yaml -l python -o python_client -c client.config.json
 rm -rf apache2
