c95d0fa76aff6d75cee686e1909d20e78783006f
Ian Wright <ian.wright@well.ox.ac.uk>
2017-11-17 11:37:16 +0000

    Change to new controller model

--- serverless/sims-backbone-location-service/location/handler.py
+++ serverless/sims-backbone-location-service/location/handler.py
@@ -19,50 +19,28 @@ if cmd_subfolder not in sys.path:
      sys.path.insert(0, cmd_subfolder)
 
 from swagger_server.models.location import Location
-from datetime import date, datetime
-from typing import List, Dict
-from six import iteritems
-#from ..util import deserialize_date, deserialize_datetime
-
-from decimal import *
 
 import logging
 
-from backbone_server.location.post import LocationPost
+from decimal import *
 
-from backbone_server.location.put import LocationPut
+from backbone_server.controllers.location_controller import LocationController
 
-from backbone_server.location.get import LocationGetById
-
-from backbone_server.location.gets import LocationsGet
-
-from backbone_server.location.delete import LocationDelete
-from backbone_server.location.get_by_name import LocationGetByPartnerName
-from backbone_server.location.get_by_gps import LocationGetByGPS
-
-from backbone_server.connect  import get_connection
-
-from backbone_server.errors.duplicate_key_exception import DuplicateKeyException
-from backbone_server.errors.missing_key_exception import MissingKeyException
-
+location_controller = LocationController()
 
 def create_location(event, context):
 
+    user = event['requestContext']['authorizer']['principalId']
+
     location = Location.from_dict(json.loads(event["body"]))
 
-    retcode = 200
-    loc = None
-
-    try:
-        post = LocationPost(get_connection())
-
-        loc = post.post(location)
-    except DuplicateKeyException as dke:
-        logging.getLogger(__name__).error("create_location: {}".format(repr(dke)))
-        retcode = 422
+    loc, retcode = location_controller.create_location(location, user)
 
     response = {
         "statusCode": retcode,
+        "headers": {
+            "Access-Control-Allow-Origin" : "*"
+        },
         "body": str(loc)
     }
 
@@ -70,22 +48,18 @@ def create_location(event, context):
 
 def download_location(event, context):
 
+    user = event['requestContext']['authorizer']['principalId']
+
     if 'pathParameters' in event:
         location_id = event["pathParameters"]["location_id"]
 
-    get = LocationGetById(get_connection())
-
-    retcode = 200
-    loc = None
-
-    try:
-        loc = get.get(location_id)
-    except MissingKeyException as dme:
-        logging.getLogger(__name__).error("download_location: {}".format(repr(dme)))
-        retcode = 404
+    loc, retcode = location_controller.download_location(location_id, user)
 
     response = {
         "statusCode": retcode,
+        "headers": {
+            "Access-Control-Allow-Origin" : "*"
+        },
         "body": str(loc)
     }
 
@@ -94,8 +68,7 @@ def download_location(event, context):
 
 def download_locations(event, context):
 
-    print((event['requestContext']['authorizer']['key']))
-    print((event['requestContext']['authorizer']['principalId']))
+    user = event['requestContext']['authorizer']['principalId']
 
     study_name = None
     start =  None
@@ -112,16 +85,7 @@ def download_locations(event, context):
         if 'orderby' in event["queryStringParameters"]:
             orderby = event["queryStringParameters"]["orderby"]
 
-    get = LocationsGet(get_connection())
-
-    retcode = 200
-    loc = None
-
-    try:
-        loc = get.get(study_name, start, count, orderby)
-    except MissingKeyException as dme:
-        logging.getLogger(__name__).error("download_location: {}".format(repr(dme)))
-        retcode = 404
+    loc, retcode = location_controller.download_locations(study_name, start, count, orderby, user)
 
     response = {
         "statusCode": retcode,
@@ -136,33 +100,20 @@ def download_locations(event, context):
 
 def update_location(event, context):
 
+    user = event['requestContext']['authorizer']['principalId']
+
     if 'pathParameters' in event:
         location_id = event["pathParameters"]["location_id"]
 
     location = Location.from_dict(json.loads(event["body"]))
 
-    retcode = 200
-    loc = None
-
-    try:
-        put = LocationPut(get_connection())
-
-        loc = put.put(location_id, location)
-    except DuplicateKeyException as dke:
-        logging.getLogger(__name__).error("update_location: {}".format(repr(dke)))
-        retcode = 422
-    except MissingKeyException as dme:
-        logging.getLogger(__name__).error("update_location: {}".format(repr(dme)))
-        retcode = 404
-
-
-    body = {
-        "message": "Update Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
+    loc, retcode = location_controller.update_location(location_id, location, user)
 
     response = {
         "statusCode": retcode,
+        "headers": {
+            "Access-Control-Allow-Origin" : "*"
+        },
         "body": str(loc)
     }
 
@@ -170,28 +121,22 @@ def update_location(event, context):
 
 def download_gps_location(event, context):
 
+    user = event['requestContext']['authorizer']['principalId']
+
     if 'pathParameters' in event:
         latitude = event["pathParameters"]["latitude"]
         longitude = event["pathParameters"]["longitude"]
 
-    get = LocationGetByGPS(get_connection())
+    lat = Decimal(latitude)
+    lng = Decimal(longitude)
 
-    retcode = 200
-    loc = None
-
-    try:
-        lat = Decimal(latitude)
-        lng = Decimal(longitude)
-        loc = get.get(lat, lng)
-    except MissingKeyException as dme:
-        logging.getLogger(__name__).error("download_partner_location: {}".format(repr(dme)))
-        retcode = 404
-    except InvalidOperation as nfe:
-        logging.getLogger(__name__).error("download_partner_location: {}".format(repr(nfe)))
-        retcode = 422
+    loc, retcode = location_controller.download_gps_location(lat, lng, user)
 
     response = {
         "statusCode": retcode,
+        "headers": {
+            "Access-Control-Allow-Origin" : "*"
+        },
         "body": str(loc)
     }
 
