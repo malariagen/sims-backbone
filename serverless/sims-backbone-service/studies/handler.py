0766ed8169fce31c4e2c53d8fe071239d9a73b99
Ian Wright <ian.wright@well.ox.ac.uk>
2017-11-20 09:46:05 +0000

    Change python import PATH - note PYTHON_PATH in the environment does not work

--- serverless/sims-backbone-service/studies/handler.py
+++ serverless/sims-backbone-service/studies/handler.py
@@ -6,15 +6,11 @@ if cmd_folder not in sys.path:
      sys.path.insert(0, cmd_folder)
 
 # Use this if you want to include modules from a subfolder
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe() ))[0],"server")))
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"../server")))
 if cmd_subfolder not in sys.path:
      sys.path.insert(0, cmd_subfolder)
 
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"server/bb_server")))
-if cmd_subfolder not in sys.path:
-     sys.path.insert(0, cmd_subfolder)
-
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"dev")))
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile(inspect.currentframe()))[0],"../server/bb_server")))
 if cmd_subfolder not in sys.path:
      sys.path.insert(0, cmd_subfolder)
 
