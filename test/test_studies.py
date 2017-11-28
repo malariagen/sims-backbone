d264e050eeedba677f8139cbd7ccdd95e33916ee
Ian Wright <ian.wright@well.ox.ac.uk>
2017-11-28 14:40:18 +0000

    Make test study names unique

--- test/test_studies.py
+++ test/test_studies.py
@@ -100,7 +100,7 @@ class TestStudies(TestBase):
         try:
 
             with self.assertRaises(Exception) as context:
-                study1 = study_api.download_study('7777-MD-UP')
+                study1 = study_api.download_study('2003-MD-UP')
             self.assertEqual(context.exception.status, 404)
 
         except ApiException as error:
@@ -115,22 +115,22 @@ class TestStudies(TestBase):
 
         try:
 
-            samp = swagger_client.SamplingEvent(None, '0003-MD-UP', date(2017, 10, 10),
+            samp = swagger_client.SamplingEvent(None, '2004-MD-UP', date(2017, 10, 10),
                                                 doc_accuracy = 'month',
                                                 partner_species = 'P. falciparum')
             created = api_instance.create_sampling_event(samp)
 
-            study1 = study_api.download_study('0003-MD-UP')
+            study1 = study_api.download_study('2004-MD-UP')
 
             self.assertEqual(study1.partner_species[0].partner_species, 'P. falciparum', 'Species not set')
             #Taxa or study name are the only things you can update
             #partner_species belong to the sampling event
-            study1.name = '0003-PF-MD-UP'
+            study1.name = '2004-PF-MD-UP'
             taxa = swagger_client.Taxonomy(5833)
 
             study1.partner_species[0].taxa = [ taxa ]
-            study2 = study_api.update_study('0003-MD-UP', study1)
-            self.assertEqual(study2.name,'0003-PF-MD-UP')
+            study2 = study_api.update_study('2004-MD-UP', study1)
+            self.assertEqual(study2.name,'2004-PF-MD-UP')
             self.assertEqual(study2.partner_species[0].taxa[0].taxonomy_id, 5833, 'taxa not updated')
 
             api_instance.delete_sampling_event(created.sampling_event_id)
@@ -147,12 +147,12 @@ class TestStudies(TestBase):
 
         try:
 
-            samp = swagger_client.SamplingEvent(None, '0004-MD-UP', date(2017, 10, 10),
+            samp = swagger_client.SamplingEvent(None, '2005-MD-UP', date(2017, 10, 10),
                                                 doc_accuracy = 'month',
                                                 partner_species = 'P. falciparum')
             created = api_instance.create_sampling_event(samp)
 
-            study1 = study_api.download_study('0004-MD-UP')
+            study1 = study_api.download_study('2005-MD-UP')
 
             self.assertEqual(study1.partner_species[0].partner_species, 'P. falciparum', 'Species not set')
             #Taxa or study name are the only things you can update
@@ -161,7 +161,7 @@ class TestStudies(TestBase):
 
             study1.partner_species[0].taxa = [ taxa ]
             with self.assertRaises(Exception) as context:
-                study2 = study_api.update_study('0004-MD-UP', study1)
+                study2 = study_api.update_study('2004-MD-UP', study1)
             self.assertEqual(context.exception.status, 422)
             #self.assertEqual(study2.partner_species[0].taxa[0].taxonomy_id, 999999, 'taxa not updated')
 
@@ -179,12 +179,12 @@ class TestStudies(TestBase):
 
         try:
 
-            samp = swagger_client.SamplingEvent(None, '0005-MD-UP', date(2017, 10, 10),
+            samp = swagger_client.SamplingEvent(None, '2006-MD-UP', date(2017, 10, 10),
                                                 doc_accuracy = 'month',
                                                 partner_species = 'P. falciparum')
             created = api_instance.create_sampling_event(samp)
 
-            events = api_instance.download_sampling_events_by_study('0005-MD-UP')
+            events = api_instance.download_sampling_events_by_study('2006-MD-UP')
 
             self.assertEqual(events.count, 1, "Event expected")
 
@@ -205,7 +205,7 @@ class TestStudies(TestBase):
         try:
 
             with self.assertRaises(Exception) as context:
-                events = api_instance.download_sampling_events_by_study('0006-MD-UP')
+                events = api_instance.download_sampling_events_by_study('2007-MD-UP')
             self.assertEqual(context.exception.status, 404)
 
 
