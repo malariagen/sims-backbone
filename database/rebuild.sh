5cf1c67d3432f334cd84c89beab7de901d568407
Ian Wright <ian.wright@well.ox.ac.uk>
2018-01-12 09:48:22 +0000

    Reworking of dump/rebuild scripts

--- database/rebuild.sh
+++ database/rebuild.sh
@@ -1,12 +1,14 @@
+pg_dump backbone_service -s > /tmp/backbone_service.psql
 sudo -u postgres psql << +++EOF
 DROP database backbone_service;
 CREATE DATABASE backbone_service;
 Alter DATABASE backbone_service OWNER TO iwright;
 ALTER DATABASE backbone_service SET search_path=public, backbone_service, contrib;
 \connect backbone_service;
-\i backbone_service.psql;
+\i /tmp/backbone_service.psql;
 \connect backbone_service;
 SELECT postgis_full_version();
 \copy countries (English, French, alpha2, alpha3, numeric_code) FROM './country_codes.tsv' DELIMITER E'\t'  HEADER CSV;
 \copy taxonomies (id, rank, name) FROM './taxa.tsv' DELIMITER E'\t'  HEADER CSV;
 +++EOF
+rm /tmp/backbone_service.psql
