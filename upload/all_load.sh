3fa4ef29c032a580cff5017a4a750c40bb33bfed
Ian Wright <ian.wright@well.ox.ac.uk>
2018-01-17 11:06:43 +0000

    Replace ROMA upload

--- upload/all_load.sh
+++ upload/all_load.sh
@@ -2,11 +2,11 @@
 ./runsss.sh 2017_06_07_report_sample_status.xls
 ./run.sh pf_6_metadata.txt pf_6_metadata.json
 ./run.sh pv_3.denormalized.txt pv_3_sanger_source_code_metadata.json
-#./run.sh manage_sites.denormalized.txt denorm_manage_sites.json
 ./run.sh pv_3_broad_metadata.txt pv_3_broad_metadata.json
-./run.sh genre.dump_samples.tsv genre.json
-./run.sh spotmalaria.dump_samples.tsv spotmalaria.dump_samples.json
-./run.sh vivax.dump_samples.tsv vivax.json
-./run.sh vobs.dump_samples.tsv vobs.json
+for i in ../../ROMA_Deploy/data/genre_dump.20180116103346.json ../../ROMA_Deploy/data/vivax_dump.20180116103346.json ../../ROMA_Deploy/data/spotmalaria_dump.20180116103346.json ../../ROMA_Deploy/data/vobs_dump.20180116103346.json
+do
+    ./run_roma.sh $i
+done
 ./run.sh ag1000g.samples.meta.txt ag1000g.json
 ./run_load_country_oxford.sh
+./run.sh manage_sites.denormalized.txt denorm_manage_sites.json
