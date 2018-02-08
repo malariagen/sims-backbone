5cc370790bc7369882cb57c0c3274958c9c1acf1
Ian Wright <ian.wright@well.ox.ac.uk>
2018-02-08 15:37:19 +0000

    First location tests

--- upload/test/all_tests.py
+++ upload/test/all_tests.py
@@ -3,6 +3,7 @@ import unittest
 from test_sampling_event import TestSampling_Event
 from test_date import TestDate
 from test_roma import TestROMA
+from test_location import TestLocation
 
 
 
@@ -12,6 +13,7 @@ def my_suite():
     suite.addTest(unittest.makeSuite(TestSampling_Event))
     suite.addTest(unittest.makeSuite(TestDate))
     suite.addTest(unittest.makeSuite(TestROMA))
+    suite.addTest(unittest.makeSuite(TestLocation))
 
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
---
+++ upload/test/test_location.py
@@ -0,0 +1,146 @@
+from test_base import TestBase
+
+import difflib
+
+import datetime
+import json
+
+from uploader import Uploader
+
+import swagger_client
+from swagger_client.rest import ApiException
+
+class TestLocation(TestBase):
+
+
+    _locations = []
+
+    """
+    """
+    @classmethod
+    def setUpClass(self):
+
+        super(TestLocation, self).setUpClass()
+        sd = Uploader(self._config_file)
+        sd.use_message_buffer = True
+        json_data = json.loads('''{
+            "values": {
+                "sample_oxford_id": {
+                    "column": 0,
+                    "type": "string"
+                },
+                "study_id": {
+                    "column": 1,
+                    "type": "string"
+                },
+                "latitude": {
+                    "column": 2,
+                    "type": "float"
+                },
+                "longitude": {
+                    "column": 3,
+                    "type": "float"
+                },
+                "location_name": {
+                    "column": 4,
+                    "type": "string"
+                },
+                "country": {
+                    "column": 5,
+                    "type": "string"
+                },
+                "proxy_latitude": {
+                    "column": 6,
+                    "type": "float"
+                },
+                "proxy_longitude": {
+                    "column": 7,
+                    "type": "float"
+                },
+                "proxy_location_name": {
+                    "column": 8,
+                    "type": "string"
+                }
+            }
+        }''')
+        sd.load_data_file(json_data, 'locations.tsv')
+
+        self._messages = sd.message_buffer
+
+    """
+    """
+    @classmethod
+    def tearDownClass(self):
+
+        event_api_instance = swagger_client.SamplingEventApi(self._api_client)
+
+        for study in ['9040', '9041', '9042']:
+            test_events = event_api_instance.download_sampling_events_by_study(study)
+
+            for event in test_events.sampling_events:
+                event_api_instance.delete_sampling_event(event.sampling_event_id)
+
+        location_api_instance = swagger_client.LocationApi(self._api_client)
+
+        api_instance = swagger_client.EventSetApi(self._api_client)
+
+        api_instance.delete_event_set('locations')
+
+        for loc in self._locations:
+            location_api_instance.delete_location(loc)
+
+
+    """
+    """
+    def test_location_duplicate_name(self):
+
+        api_instance = swagger_client.SamplingEventApi(self._api_client)
+
+        try:
+            looked_up = api_instance.download_sampling_event_by_identifier('oxford_id', '22345')
+            errmsg = "Location name conflict\t9040\tRatanakiri\t{'accuracy': None,\n 'country': 'KHM',\n 'curated_name': None,\n 'curation_method': None,\n 'identifiers': [" +\
+                    "{'identifier_source': 'locations',\n                  " +\
+                    "'identifier_type': 'partner_name',\n                  " +\
+                    "'identifier_value': 'Ratanakiri',\n                  'study_name': '9040'}" +\
+                "],\n 'latitude': 13.86208,\n " +\
+"'location_id': '{}'".format(looked_up.location_id) +\
+",\n 'longitude': 107.097015,\n 'notes': 'locations.tsv'}\t13.9\t107.1\t[('country', 'KHM'), ('latitude', '13.86208'), ('location_name', 'Ratanakiri'), ('longitude', '107.097015'), ('proxy_latitude', '13.9'), ('proxy_location_name', 'Ratanakiri'), ('proxy_longitude', '107.1'), ('sample_oxford_id', '22345'), ('study_id', '9040 Upload location test study')]"
+            #Used for diagnosis - may not be _messages[0]
+            #print('\n'.join(difflib.context_diff(errmsg.split('\n'), self._messages[0].split('\n'))))
+            self.assertIn(errmsg, self._messages)
+            self.assertIsNone(looked_up.proxy_location_id)
+            self.assertIsNone(looked_up.proxy_location)
+
+            if looked_up.location_id not in self._locations:
+                self._locations.append(looked_up.location_id)
+
+        except ApiException as error:
+            self.fail("test_location_duplicate_name: Exception when calling download_sampling_event_by_identifier {}"
+                        .format(error))
+
+
+    """
+    """
+    def test_location_duplicate_name_ok(self):
+
+        api_instance = swagger_client.SamplingEventApi(self._api_client)
+
+        try:
+            looked_up = api_instance.download_sampling_event_by_identifier('oxford_id', '22346')
+
+            self.assertIsNotNone(looked_up.location_id)
+
+            if looked_up.location_id not in self._locations:
+                self._locations.append(looked_up.location_id)
+
+            looked_up = api_instance.download_sampling_event_by_identifier('oxford_id', '22347')
+
+            self.assertIsNotNone(looked_up.location_id)
+
+            if looked_up.location_id not in self._locations:
+                self._locations.append(looked_up.location_id)
+
+        except ApiException as error:
+            self.fail("test_location_duplicate_name: Exception when calling download_sampling_event_by_identifier {}"
+                        .format(error))
+
