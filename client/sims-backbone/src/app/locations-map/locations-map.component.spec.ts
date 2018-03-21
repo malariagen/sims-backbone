a2283b62b72b0490614de847522781fc60e5d856
Ian Wright <ian.wright@well.ox.ac.uk>
2018-03-21 15:14:13 +0000

    Test show/hide all/no layers buttons

--- client/sims-backbone/src/app/locations-map/locations-map.component.spec.ts
+++ client/sims-backbone/src/app/locations-map/locations-map.component.spec.ts
@@ -4,6 +4,7 @@ import { LocationsMapComponent } from './locations-map.component';
 import { Component, Directive, EventEmitter, Input, Output } from '@angular/core';
 import { LatLng, LatLngBounds } from '@agm/core';
 import { LeafletDirective } from '@asymmetrik/ngx-leaflet';
+import { Locations, Location } from '../typescript-angular-client';
 
 @Directive({
   selector: '[leafletLayersControl]'
@@ -38,6 +39,48 @@ describe('LocationsMapComponent', () => {
   });
 
   it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should show buttons', () => {
+
+    component.locations = <Locations> {
+      count: 2,
+      locations: [
+        <Location> {
+          latitude: 0,
+          longitude: 0
+        },
+        <Location> {
+          latitude: 0,
+          longitude: 0
+        }
+      ]
+    };
+    fixture.detectChanges();
+    expect(component._locations.count).toBe(2);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#noLayers').textContent).toBe('No layers');
+    expect(compiled.querySelector('#allLayers').textContent).toBe('All layers');
+    expect(component).toBeTruthy();
+  });
+
+  it('should hide buttons', () => {
+
+    component.locations = <Locations> {
+      count: 1,
+      locations: [
+        <Location> {
+          latitude: 0,
+          longitude: 0
+        }
+      ]
+    };
+    fixture.detectChanges();
+    expect(component._locations.count).toBe(1);
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('#noLayers')).toBeNull();
+    expect(compiled.querySelector('#allLayers')).toBeNull();
     expect(component).toBeTruthy();
   });
 });
