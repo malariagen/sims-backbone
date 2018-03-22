22826bd7db0fcd876a3a058b68076fbfc59feb80
Ian Wright <ian.wright@well.ox.ac.uk>
2018-03-22 09:58:34 +0000

    Enhanced species from solaris

--- upload/all_load.sh
+++ upload/all_load.sh
@@ -1,4 +1,5 @@
 ./run.sh oxford_11JAN2018.csv oxford.json
+./run.sh solaris_species.csv solaris_species.json
 ./runsss.sh 2017_06_07_report_sample_status.xls
 ./run.sh pf_6_metadata.txt pf_6_metadata.json
 ./run.sh pv_3.denormalized.txt pv_3_sanger_source_code_metadata.json
