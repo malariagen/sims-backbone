222ba02e03ccf360d457d8b6411205de5a49da34
Ian Wright <ian.wright@well.ox.ac.uk>
2018-05-01 09:48:17 +0100

    Update files for upload

--- upload/all_load.sh
+++ upload/all_load.sh
@@ -1,9 +1,9 @@
 ./run.sh oxford_11JAN2018.csv oxford.json
+./run.sh Resolved_study_codes_27APR2018.txt resolved_studies.json
 ./run.sh solaris_species.csv solaris_species.json
 ./runsss.sh 2017_06_07_report_sample_status.xls
 ./run.sh pf_6_metadata.txt pf_6_metadata.json
-./run.sh pv_3.denormalized.txt pv_3_sanger_source_code_metadata.json
-./run.sh pv_3_broad_metadata.txt pv_3_broad_metadata.json
+./run.sh Pv4_manifest_20180326.txt pv_4_manifest.json
 for i in ../../ROMA_Deploy/data/genre_dump.20180116103346.json ../../ROMA_Deploy/data/vivax_dump.20180116103346.json ../../ROMA_Deploy/data/spotmalaria_dump.20180116103346.json ../../ROMA_Deploy/data/vobs_dump.20180116103346.json
 do
     ./run_roma.sh $i
