ada2298362c8493d6c58a0c7d55838e1beb1a914
Ian Wright <ian.wright@well.ox.ac.uk>
2018-06-27 11:50:29 +0100

    Utility script to check all paths in swagger.yaml are defined in serverless.yml

---
+++ serverless/check_functions.sh
@@ -0,0 +1,15 @@
+# vim: set textwidth=0 wrapmargin=0:
+for i in $(grep '/' ../swagger.yaml | grep :$ | sed -e 's/:$//' -e "s/'//g" -e 's/^\s//' -e 's/{\([a-z_]*\)\([A-Z].\)/{\1_\l\2/g' -e 's#\/##' -e 's/\([A-Z].\)\([a-z]*\)}/_\l\1\2}/g' )
+do
+    SEARCH="path: ${i}$"
+    egrep -q "${SEARCH}" */serverless.yml
+    if [ $? -eq 1 ]
+    then
+        SEARCH="path: sims-backbone-service/v1/${i}$"
+        egrep -q "${SEARCH}" */serverless.yml
+        if [ $? -eq 1 ]
+        then
+            echo "Missing $i"
+        fi
+    fi
+done
