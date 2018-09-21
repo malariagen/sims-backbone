1d34791f7ad7833260265fcc56e02be333f154aa
Ian Wright <ian.wright@well.ox.ac.uk>
2018-09-21 13:55:49 +0100

    serverless change derived to derivative

--- serverless/derivative_sample_service/derivative_sample/handler.py
+++ serverless/derivative_sample_service/derivative_sample/handler.py
@@ -14,57 +14,57 @@ cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(insp
 if cmd_subfolder not in sys.path:
      sys.path.insert(0, cmd_subfolder)
 
-from swagger_server.models.derived_sample import DerivedSample
+from swagger_server.models.derivative_sample import DerivativeSample
 
 import logging
 
 import datetime
 
-from backbone_server.controllers.derived_sample_controller import DerivedSampleController
+from backbone_server.controllers.derivative_sample_controller import DerivativeSampleController
 
 from util.response_util import create_response
 
-derived_sample_controller = DerivedSampleController()
+derivative_sample_controller = DerivativeSampleController()
 
-def create_derived_sample(event, context):
+def create_derivative_sample(event, context):
 
     user = event['requestContext']['authorizer']['principalId']
 
-    derived_sample = DerivedSample.from_dict(json.loads(event["body"]))
+    derivative_sample = DerivativeSample.from_dict(json.loads(event["body"]))
 
-    value, retcode = derived_sample_controller.create_derived_sample(derived_sample, user,
-                                                                       derived_sample_controller.authorizer(event['requestContext']['authorizer']))
+    value, retcode = derivative_sample_controller.create_derivative_sample(derivative_sample, user,
+                                                                       derivative_sample_controller.authorizer(event['requestContext']['authorizer']))
 
     return create_response(event, retcode, value)
 
 
-def delete_derived_sample(event, context):
-
-    user = event['requestContext']['authorizer']['principalId']
-
-    if 'pathParameters' in event:
-        derived_sample_id = event["pathParameters"]["derived_sample_id"]
-
-    value, retcode =  derived_sample_controller.delete_derived_sample(derived_sample_id, user,
-                                                                        derived_sample_controller.authorizer(event['requestContext']['authorizer']))
-
-    return create_response(event, retcode, value)
-
-
-def download_derived_sample(event, context):
+def delete_derivative_sample(event, context):
 
     user = event['requestContext']['authorizer']['principalId']
 
     if 'pathParameters' in event:
-        derived_sample_id = event["pathParameters"]["derived_sample_id"]
+        derivative_sample_id = event["pathParameters"]["derivative_sample_id"]
 
-    value, retcode =  derived_sample_controller.download_derived_sample(derived_sample_id, user,
-                                                                          derived_sample_controller.authorizer(event['requestContext']['authorizer']))
+    value, retcode =  derivative_sample_controller.delete_derivative_sample(derivative_sample_id, user,
+                                                                        derivative_sample_controller.authorizer(event['requestContext']['authorizer']))
 
     return create_response(event, retcode, value)
 
 
-def download_derived_samples_by_attr(event, context):
+def download_derivative_sample(event, context):
+
+    user = event['requestContext']['authorizer']['principalId']
+
+    if 'pathParameters' in event:
+        derivative_sample_id = event["pathParameters"]["derivative_sample_id"]
+
+    value, retcode =  derivative_sample_controller.download_derivative_sample(derivative_sample_id, user,
+                                                                          derivative_sample_controller.authorizer(event['requestContext']['authorizer']))
+
+    return create_response(event, retcode, value)
+
+
+def download_derivative_samples_by_attr(event, context):
 
     user = event['requestContext']['authorizer']['principalId']
 
@@ -77,16 +77,16 @@ def download_derived_samples_by_attr(event, context):
         if 'study_name' in event["queryStringParameters"]:
             study_name = event["queryStringParameters"]["study_name"]
 
-    value, retcode = derived_sample_controller.download_derived_samples_by_attr(prop_name,
+    value, retcode = derivative_sample_controller.download_derivative_samples_by_attr(prop_name,
                                                                                   prop_value,
                                                                                   study_name,
                                                                                   user,
-                                                                                  derived_sample_controller.authorizer(event['requestContext']['authorizer']))
+                                                                                  derivative_sample_controller.authorizer(event['requestContext']['authorizer']))
 
 
     return create_response(event, retcode, value)
 
-def download_derived_samples_by_os_attr(event, context):
+def download_derivative_samples_by_os_attr(event, context):
 
     user = event['requestContext']['authorizer']['principalId']
 
@@ -99,27 +99,27 @@ def download_derived_samples_by_os_attr(event, context):
         if 'study_name' in event["queryStringParameters"]:
             study_name = event["queryStringParameters"]["study_name"]
 
-    value, retcode = derived_sample_controller.download_derived_samples_by_os_attr(prop_name,
+    value, retcode = derivative_sample_controller.download_derivative_samples_by_os_attr(prop_name,
                                                                                      prop_value,
                                                                                      study_name,
                                                                                      user,
-                                                                                     derived_sample_controller.authorizer(event['requestContext']['authorizer']))
+                                                                                     derivative_sample_controller.authorizer(event['requestContext']['authorizer']))
 
 
     return create_response(event, retcode, value)
 
-def update_derived_sample(event, context):
+def update_derivative_sample(event, context):
 
     user = event['requestContext']['authorizer']['principalId']
 
     if 'pathParameters' in event:
-        derived_sample_id = event["pathParameters"]["derived_sample_id"]
+        derivative_sample_id = event["pathParameters"]["derivative_sample_id"]
 
-    derived_sample = DerivedSample.from_dict(json.loads(event["body"]))
+    derivative_sample = DerivativeSample.from_dict(json.loads(event["body"]))
 
-    value, retcode = derived_sample_controller.update_location(derived_sample_id, derived_sample,
+    value, retcode = derivative_sample_controller.update_location(derivative_sample_id, derivative_sample,
                                                                 user,
-                                                                derived_sample_controller.authorizer(event['requestContext']['authorizer']))
+                                                                derivative_sample_controller.authorizer(event['requestContext']['authorizer']))
 
     return create_response(event, retcode, value)
 
