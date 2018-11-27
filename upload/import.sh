9297f68447636dd5b5d632fd9d2d878605761624
Ian Wright <ian.wright@well.ox.ac.uk>
2018-11-27 12:02:02 +0000

    Tweaks to import script

--- upload/import.sh
+++ upload/import.sh
@@ -1,7 +1,8 @@
 test -f environment && source environment
+test -f ${INPUT1_STAGING_DIR}/import/environment && source ${INPUT1_STAGING_DIR}/import/environment
 for i in ${INPUT1_STAGING_DIR}/import/roma/*
 do
-    python3 upload_roma.py ${IMPORT_CONFIG} $1 $2
-    mv ${i} ${INPUT1_STAGING_DIR}/archive/roma/$(basename ${i}).$(date +%Y-%m-%d:%H:%M:%S)
+    python3 upload_roma.py ${IMPORT_CONFIG} $i && \
+        mv ${i} ${INPUT1_STAGING_DIR}/archive/roma/$(basename ${i}).$(date +%Y-%m-%d:%H:%M:%S)
 done
 OUTPUT1_STAGING_DIR=/data/output
