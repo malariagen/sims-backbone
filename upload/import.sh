3616f9c2d94680e6ef0b73021d4429a3472ae393
Ian Wright <ian.wright@well.ox.ac.uk>
2019-01-03 13:29:54 +0000

    Make sure archive dir exists before mv

--- upload/import.sh
+++ upload/import.sh
@@ -6,11 +6,14 @@ OUTFILE=${OUTPUT1_STAGING_DIR}/import_$(date +%Y-%m-%d:%H:%M:%S).log
 exec > >(tee -i ${OUTFILE})
 exec 2>&1
 
+ARCHIVE_DIR=${INPUT1_STAGING_DIR}/archive
+
 shopt -s nullglob
 for i in ${INPUT1_STAGING_DIR}/import/roma/*
 do
+    test -d ${ARCHIVE_DIR}/roma || mkdir -p ${ARCHIVE_DIR}/roma
     python3 upload_roma.py ${IMPORT_CONFIG} $i && \
-        mv ${i} ${INPUT1_STAGING_DIR}/archive/roma/$(basename ${i}).$(date +%Y-%m-%d:%H:%M:%S)
+        mv ${i} ${ARCHIVE_DIR}/roma/$(basename ${i}).$(date +%Y-%m-%d:%H:%M:%S)
 done
 python3 set_taxa.py ${IMPORT_CONFIG}
 python3 set_studies.py ${IMPORT_CONFIG} ${CMIS_CONFIG}
