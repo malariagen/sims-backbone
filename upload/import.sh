7fc73d75649f267dc3c68dd6168603912e90e7fc
Ian Wright <ian.wright@well.ox.ac.uk>
2019-01-07 09:54:50 +0000

    More import tweaks

--- upload/import.sh
+++ upload/import.sh
@@ -5,7 +5,8 @@ test -f ${INPUT1_STAGING_DIR}/import/environment && source ${INPUT1_STAGING_DIR}
 
 # Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
 OUTFILE=${OUTPUT1_STAGING_DIR}/import_$(date +%Y-%m-%d:%H:%M:%S).log
-exec &> >(tee -i ${OUTFILE})
+#Doesn't seem to work when run as part of data pipeline so do more simply for each action
+#exec &> >(tee -i ${OUTFILE})
 
 ARCHIVE_DIR=${OUTPUT1_STAGING_DIR}/archive
 
@@ -13,8 +14,8 @@ shopt -s nullglob
 for i in ${INPUT1_STAGING_DIR}/import/roma/*
 do
     test -d ${ARCHIVE_DIR}/roma || mkdir -p ${ARCHIVE_DIR}/roma
-    python3 upload_roma.py ${IMPORT_CONFIG} $i && \
-        mv ${i} ${ARCHIVE_DIR}/roma/$(basename ${i}).$(date +%Y-%m-%d:%H:%M:%S)
+    python3 upload_roma.py ${IMPORT_CONFIG} $i 2>&1 | tee -a ${OUTFILE}
+    mv ${i} ${ARCHIVE_DIR}/roma/$(basename ${i}).$(date +%Y-%m-%d:%H:%M:%S)
 done
-python3 set_taxa.py ${IMPORT_CONFIG}
-python3 set_studies.py ${IMPORT_CONFIG} ${CMIS_CONFIG}
+python3 set_taxa.py ${IMPORT_CONFIG} 2>&1 | tee -a ${OUTFILE}
+python3 set_studies.py ${IMPORT_CONFIG} ${CMIS_CONFIG} 2>&1 | tee -a ${OUTFILE}
