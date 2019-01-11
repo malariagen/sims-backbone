0eb08b1eba445ef5aef505233323e38876f64508
Ian Wright <ian.wright@well.ox.ac.uk>
2019-01-11 10:50:47 +0000

    Another attempt to solve the mystery of the import output

--- upload/import.sh
+++ upload/import.sh
@@ -5,8 +5,9 @@ test -f ${INPUT1_STAGING_DIR}/import/environment && source ${INPUT1_STAGING_DIR}
 
 # Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
 OUTFILE=${OUTPUT1_STAGING_DIR}/import_$(date +%Y-%m-%d:%H:%M:%S).log
-#Doesn't seem to work when run as part of data pipeline so do more simply for each action
-#exec &> >(tee -i ${OUTFILE})
+OUTFILE1=${OUTPUT1_STAGING_DIR}/import_$(date +%Y-%m-%d:%H:%M:%S).log1
+echo "Hello" > ${OUTFILE1}
+exec &> > ${OUTFILE}
 
 ARCHIVE_DIR=${OUTPUT1_STAGING_DIR}/archive
 
@@ -14,11 +15,11 @@ shopt -s nullglob
 for i in ${INPUT1_STAGING_DIR}/import/roma/*
 do
     test -d ${ARCHIVE_DIR}/roma || mkdir -p ${ARCHIVE_DIR}/roma
-    python3 upload_roma.py ${IMPORT_CONFIG} $i 2>&1 | tee -a ${OUTFILE}
+    python3 upload_roma.py ${IMPORT_CONFIG} $i
     mv ${i} ${ARCHIVE_DIR}/roma/$(basename ${i}).$(date +%Y-%m-%d:%H:%M:%S)
 done
-python3 set_taxa.py ${IMPORT_CONFIG} 2>&1 | tee -a ${OUTFILE}
-python3 set_studies.py ${IMPORT_CONFIG} ${CMIS_CONFIG} 2>&1 | tee -a ${OUTFILE}
+python3 set_taxa.py ${IMPORT_CONFIG}
+python3 set_studies.py ${IMPORT_CONFIG} ${CMIS_CONFIG}
 if [ ${INPUT1_STAGING_DIR} = "s3://malariagen-sims-import-test/" ]
 then
     ENVIRON=test
