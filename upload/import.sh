e8a343519417118f72cb06e0030b38daac3fce6e
Ian Wright <ian.wright@well.ox.ac.uk>
2019-01-14 08:15:23 +0000

    More changes to import log handling

--- upload/import.sh
+++ upload/import.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+ENVIRON=$1
+
+if [ -z ${ENVIRON} ]
+    ENVIRON=dev
+fi
 test -f environment && source environment
 test -f ${INPUT1_STAGING_DIR}/import/environment && source ${INPUT1_STAGING_DIR}/import/environment
 
@@ -13,18 +18,15 @@ shopt -s nullglob
 for i in ${INPUT1_STAGING_DIR}/import/roma/*
 do
     test -d ${ARCHIVE_DIR}/roma || mkdir -p ${ARCHIVE_DIR}/roma
-    python3 upload_roma.py ${IMPORT_CONFIG} $i
-    mv ${i} ${ARCHIVE_DIR}/roma/$(basename ${i}).$(date +%Y-%m-%d:%H:%M:%S)
+    ROMA_LOG=${OUTPUT1_STAGING_DIR}/import_$(date +%Y-%m-%d-%H%M%S).log
+    python3 upload_roma.py ${IMPORT_CONFIG} $i 2>&1 | tee -i ${ROMA_LOG}
+    python3 upload_log.py ${CMIS_CONFIG} ${ENVIRON} ${ROMA_LOG}
+    #INPUT_STAGING_DIR is a copy so changes aren't synced
+    cp ${i} ${ARCHIVE_DIR}/roma/$(basename ${i}).$(date +%Y-%m-%d:%H:%M:%S)
+    aws s3 rm "s3://malariagen-sims-import-dev/import/roma/$(basename ${i})"
 done
 python3 set_taxa.py ${IMPORT_CONFIG}
 python3 set_studies.py ${IMPORT_CONFIG} ${CMIS_CONFIG}
-if [ ${INPUT1_STAGING_DIR} = "s3://malariagen-sims-import-test/" ]
-then
-    ENVIRON=test
-elif [ ${INPUT1_STAGING_DIR} = "s3://malariagen-sims-import-production/" ]
-then
-    ENVIRON=production
-else
-    ENVIRON=dev
-fi
-python3 upload_log.py ${CMIS_CONFIG} ${ENVIRON} ${OUTFILE}
+#Could upload the overall log but just keeping in S3 and uploading individual logs instead
+#Not uploading set_taxa and set_studies
+#python3 upload_log.py ${CMIS_CONFIG} ${ENVIRON} ${OUTFILE}
