18e3541942c365683d0d9f224bf57260e167509c
Ian Wright <ian.wright@well.ox.ac.uk>
2019-02-15 10:44:36 +0000

    Don't upload empty files

--- upload/import.sh
+++ upload/import.sh
@@ -22,14 +22,20 @@ do
     INSTANCE=$(basename ${i} | awk -F_ '{print $1}')
     ROMA_LOG=${OUTPUT1_STAGING_DIR}/${INSTANCE}_$(date +%Y-%m-%d-%H%M%S).log
     python3 upload_roma.py ${IMPORT_CONFIG} $i 2>&1 | tee -i ${ROMA_LOG}
-    python3 upload_log.py ${CMIS_CONFIG} ${ENVIRON} ${ROMA_LOG}
+    if [ -s ${ROMA_LOG} ]
+    then
+        python3 upload_log.py ${CMIS_CONFIG} ${ENVIRON} ${ROMA_LOG}
+    fi
     #INPUT_STAGING_DIR is a copy so changes aren't synced
     aws s3 mv "s3://malariagen-sims-import-${ENVIRON}/import/roma/$(basename ${i})" "s3://malariagen-sims-import-${ENVIRON}/output/archive/roma/$(basename ${i})"
 done
 METADATA_LOG=${OUTPUT1_STAGING_DIR}/metadata_$(date +%Y-%m-%d-%H%M%S).log
 python3 set_taxa.py ${IMPORT_CONFIG} | tee -i ${METADATA_LOG}
 python3 set_studies.py ${IMPORT_CONFIG} ${CMIS_CONFIG} | tee -i ${METADATA_LOG}
-python3 upload_log.py ${CMIS_CONFIG} ${ENVIRON} ${METADATA_LOG}
+if [ -s ${METADATA_LOG} ]
+then
+    python3 upload_log.py ${CMIS_CONFIG} ${ENVIRON} ${METADATA_LOG}
+fi
 #Could upload the overall log but just keeping in S3 and uploading individual logs instead
 #Not uploading set_taxa and set_studies
 #python3 upload_log.py ${CMIS_CONFIG} ${ENVIRON} ${OUTFILE}
