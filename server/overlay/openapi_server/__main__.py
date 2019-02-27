8fe790cf48edcac45b704e9d6fa7ca009f356837
Ian Wright <ian.wright@well.ox.ac.uk>
2019-02-27 09:28:04 +0000

    Configure auto reloading for local server

--- server/overlay/openapi_server/__main__.py
+++ server/overlay/openapi_server/__main__.py
@@ -9,7 +9,7 @@ def main():
     app = connexion.App(__name__, specification_dir='./openapi/')
     app.app.json_encoder = encoder.JSONEncoder
     app.add_api('openapi.yaml', arguments={'title': 'Backbone'})
-    app.run(port=8080)
+    app.run(port=8080, use_reloader=True)
 
 
 if __name__ == '__main__':
