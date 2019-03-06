1f3a2fa0c50b06d04eab485b7d7da0d860555c7b
Ian Wright <ian.wright@well.ox.ac.uk>
2019-03-06 13:47:35 +0000

    Move to openapi - partially working

--- serverless/check_functions.sh
+++ serverless/check_functions.sh
@@ -1,5 +1,5 @@
 # vim: set textwidth=0 wrapmargin=0:
-for i in $(grep '/' ../swagger.yaml | grep :$ | sed -e 's/:$//' -e "s/'//g" -e 's/^\s//' -e 's/{\([a-z_]*\)\([A-Z].\)/{\1_\l\2/g' -e 's#\/##' -e 's/\([A-Z].\)\([a-z]*\)}/_\l\1\2}/g' )
+for i in $(grep '/' ../openapi/sims-backbone.yaml | grep :$ | sed -e 's/:$//' -e "s/'//g" -e 's/^\s//' -e 's/{\([a-z_]*\)\([A-Z].\)/{\1_\l\2/g' -e 's#\/##' -e 's/\([A-Z].\)\([a-z]*\)}/_\l\1\2}/g' )
 do
     SEARCH="path: ${i}$"
     egrep -q "${SEARCH}" */serverless.yml
