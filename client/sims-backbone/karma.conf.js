f1c0b6de720a6c37371d180af85636546906900a
Ian Wright <ian.wright@bdi.ox.ac.uk>
2020-04-07 12:36:36 +0100

    Headless testing attempt

--- client/sims-backbone/karma.conf.js
+++ client/sims-backbone/karma.conf.js
@@ -2,41 +2,45 @@
 // https://karma-runner.github.io/0.13/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
-    },
-    
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    customLaunchers: {
-      ChromeNoSandbox: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
-    singleRun: false,
-    /** * maximum number of tries a browser will attempt in the case of a disconnection */
-    browserDisconnectTolerance: 2,
-    /** * How long will Karma wait for a message from a browser before disconnecting from it (in
-     * ms). */
-    browserNoActivityTimeout: 50000,
-  });
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma')
+        ],
+        client:{
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        coverageIstanbulReporter: {
+            dir: require('path').join(__dirname, 'coverage'), reports: [ 'html', 'lcovonly' ],
+            fixWebpackSourcePaths: true
+        },
+
+        reporters: ['progress', 'kjhtml'],
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        browsers: [
+            'ChromeHeadless',
+            'ChromeNoSandbox',
+            'Chrome'
+        ],
+        customLaunchers: {
+            ChromeNoSandbox: {
+                base: 'ChromeHeadless',
+                flags: ['--no-sandbox']
+            }
+        },
+        singleRun: false,
+        /** * maximum number of tries a browser will attempt in the case of a disconnection */
+        browserDisconnectTolerance: 2,
+        /** * How long will Karma wait for a message from a browser before disconnecting from it (in
+         * ms). */
+        browserNoActivityTimeout: 50000,
+    });
 };
